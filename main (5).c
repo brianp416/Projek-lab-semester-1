e360c9be5fe9a840fa7377fea9850af774ab8e55
brianp416 <119559501+brianp416@users.noreply.github.com>
2025-11-19 07:59:06 +0700

    Add files via upload

---
+++ main (5).c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h> 
+
+struct User{
+    char username[20];
+    char password[20]; 
+}
+
+void MenuAwal(){ 
+    printf("===============================\n");
+    printf("S H I P P E X  L O G I S T I C \n");
+    printf("===============================\n\n");
+    printf("[1] Login\n");
+    printf("[2] Register\n");
+    printf("[3] Exit\n"); 
+}
+
+Void MenuUtama(){
+    printf("===============================\n");
+    printf("S H I P P E X  L O G I S T I C \n");
+    printf("===============================\n\n");
+    printf("New Shipment Item\n"); 
+    printf("View All Items\n"); 
+    printf("Sort Item List\n"); 
+    printf("Search Item Name\n"); 
+    printf("Exit\n"); 
+}
+
+Void Out(){
+    printf("==========================\n"); 
+    printf("    Thank You For Using \n");
+    
+    printf("      See You Later\n";)
+    
+    printf("  Press Any Key To Exit\n");
+    printf("==========================\n"); 
+}
+
+int main() {
+    
+    return 0;
+}
